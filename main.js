a66ac7d1b2c864ce501db9b6d5d4cc5a61726b7f
MAC <mac@MacBook-Pro-de-MAC-2.local>
2021-04-30 10:23:18 +0200

    imprimirjs en HTML

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const boton = document.getElementById("spin");
+const resultext = document.getElementById("resultado");
 let alumnas = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
 
 function seleccion(){
     let elegida = alumnas[Math.floor(Math.random() * alumnas.length)];
-    console.log(elegida);
-   //return elegida;
+    document.getElementById("spin").innerHTML="elegida";
+    message = "La elegida es:" + elegida;
+    resultext.innerHTML = message;
+
 }
-boton.addEventListener("click",seleccion);
-//seleccion();
+boton.addEventListener("click",seleccion)
+
 
 function noRepetida(elegida){
 
