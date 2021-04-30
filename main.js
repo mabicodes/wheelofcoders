5b7dcb9d197ee7b410af85054f5a16d4feb02bb6
MAC <mac@MacBook-Pro-de-MAC-2.local>
2021-04-30 15:31:37 +0200

    no repetir

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ const boton = document.getElementById("spin");
 const resultext = document.getElementById("resultado");
 let alumnas = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
 
+
 function seleccion(){
-    let elegida = alumnas[Math.floor(Math.random() * alumnas.length)];
+  let posicion = Math.floor(Math.random() * alumnas.length);
+  let elegida = alumnas[posicion];
     document.getElementById("spin").innerHTML="elegida";
     message = "La elegida es:" + elegida;
     resultext.innerHTML = message;
-
+    alumnas.splice(posicion, 1);
+    console.log(elegida);
 }
-boton.addEventListener("click",seleccion)
+boton.addEventListener("click",seleccion);
 
 
 function noRepetida(elegida){
