3af78aaaa0128993730202756b827dbbf831c4e3
mavieuwu <mabigaming26@gmail.com>
2021-05-05 10:30:16 +0200

    cambios en main.js

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-const boton = document.getElementById("spin");
-const resultext = document.getElementById("resultado");
-let alumnas = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
 
+const boton = document.getElementById("spin"); //linkeamos el botón en HTML.
+const resultext = document.getElementById("resultado"); //Nos muestra el resultado en HTML.
+let alumnas = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
+let alumnas2 = []; //segundo Array
 
 function seleccion(){
-  let posicion = Math.floor(Math.random() * alumnas.length);
-  let elegida = alumnas[posicion];
-    document.getElementById("spin").innerHTML="elegida";
-    message = "La elegida es:" + elegida;
-    resultext.innerHTML = message;
-    alumnas.splice(posicion, 1);
-    console.log(elegida);
+  let posicion = Math.floor(Math.random() * alumnas.length); //Busca un valor aleatoria dentro del array.
+  let elegida = alumnas[posicion];                              //Convierte la posición en el nombre.
+    document.getElementById("spin").innerHTML="elegida"; //Onclick botones en HTML y display "elegida" onscreen HTML.
+    message = "La elegida es:" + elegida;                         //Variable mensaje que mostrar en el <p> en HTML.
+    resultext.innerHTML = message;                                //Imprimir por pantalla el variable "message"
 }
-boton.addEventListener("click",seleccion);
 
+boton.addEventListener("click",seleccion);          //llamar la funcion seleccion en click
 
-function noRepetida(elegida){
+function eliminaselec(){
+  alumnas2.push(elegida);                                       //Añade el valor del primer Array al segundo Array.
+  alumnas.splice(posicion, 1);                       //Elimina el elemento seleccionado del primer Array.
 
+  if (alumnas.length < 1){
+    alert("¡No quedan más participantes!");
+  }
 }
